c0de2daf56ee197811cc7d3aabcba68a52c70a6d
PHUONG <htphuong200100@gmail.com>
2022-01-14 16:03:57 +0700

    register ui

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const homeRouter = require("./home");
+const registerRouter = require("./register");
 
 module.exports = (app) => {
+  app.use("/register", registerRouter);
   app.use("/", homeRouter);
 };
