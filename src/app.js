e299835580597a0ae87422db95becf5ed14bdb18
PHUONG <htphuong200100@gmail.com>
2022-01-14 15:10:54 +0700

    setup display log and process error

--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
 const path = require("path");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const createError = require("http-errors");
+const logger = require("morgan");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+// Logger
+app.use(logger("dev"));
+
 // EJS engine
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.render("pages/home");
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(new createError.NotFound("Page Not Found"));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("pages/error", { layout: false });
 });
 
 app.listen(port, () => {
