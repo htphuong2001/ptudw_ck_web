6b1c4923fdfd839b1702b322c544c9fd7a041e5f
PHUONG <htphuong200100@gmail.com>
2022-01-14 15:00:23 +0700

    setup view and layout vs ejs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
+const path = require("path");
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
+
+// EJS engine
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/", (req, res, next) => {
-  res.send("Hello");
+  res.render("pages/home");
 });
 
 app.listen(port, () => {
